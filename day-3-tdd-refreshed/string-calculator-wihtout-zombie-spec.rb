4de641ed4a855dbf8a4eb30c403807e544b7c2c1
sk9073 <sivanandi9073@gmail.com>
2026-07-09 14:15:10 +0530

    day-3-string-calc-without-zombie-5-red

--- day-3-tdd-refreshed/string-calculator-wihtout-zombie-spec.rb
+++ day-3-tdd-refreshed/string-calculator-wihtout-zombie-spec.rb
@@ -13,4 +13,8 @@ describe StringCalculator do
   it 'allows new lines between numbers instead of commas' do
     expect(StringCalculator.new.add("1\n2,3")).to eq(6)
   end
+
+  it 'supports a custom delimiter declared on the first line' do
+    expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+  end
 end
