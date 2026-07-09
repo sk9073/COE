dcd00f8fde9f81eae020bedb9583bdc3a454395b
sk9073 <sivanandi9073@gmail.com>
2026-07-09 15:44:02 +0530

    day-3-string-calc-with-zombie-1-red-many

--- day-3-tdd-refreshed/string-calculator-with-zombie-spec.rb
+++ day-3-tdd-refreshed/string-calculator-with-zombie-spec.rb
@@ -9,4 +9,8 @@ describe StringCalculator do
     expect(StringCalculator.new.add('1')).to eq(1)
     expect(StringCalculator.new.add('1,2')).to eq(3)
   end
+
+  it 'allows new lines between numbers instead of commas' do
+    expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+  end
 end
