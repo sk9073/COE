310d4a7ecd23a72fef28d72ba64697dcd3bc91c7
sk9073 <sivanandi9073@gmail.com>
2026-07-09 13:19:03 +0530

    day-3-string-calc-without-zombie-2

--- day-3-tdd-refreshed/string-calculator-wihtout-zombie-spec.rb
+++ day-3-tdd-refreshed/string-calculator-wihtout-zombie-spec.rb
@@ -4,4 +4,8 @@ describe StringCalculator do
   it 'returns 0 for an empty string' do
     expect(StringCalculator.new.add('')).to eq(0)
   end
+
+  it 'returns sum of given input numbers' do
+    expect(StringCalculator.new.add('1')).to eq(1)    
+  end 
 end
