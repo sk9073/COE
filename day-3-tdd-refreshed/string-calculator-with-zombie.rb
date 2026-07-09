ae1569000387e4f71ee13e3e7c6789e59ddbbbc0
sk9073 <sivanandi9073@gmail.com>
2026-07-09 15:57:28 +0530

    day-3-string-calc-with-zombie-1-green-many

--- day-3-tdd-refreshed/string-calculator-with-zombie.rb
+++ day-3-tdd-refreshed/string-calculator-with-zombie.rb
@@ -9,7 +9,17 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    delimiter = /,|\n/
+    if numbers.start_with?('//')
+      first_line, numbers = numbers.split("\n", 2)
+      delimiter = first_line[2..]
+      if delimiter.start_with?('[')
+        delimiter = Regexp.union(delimiter.scan(/\[(.*?)\]/).flatten)
+      end
+    end
+
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
 
